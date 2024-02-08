c2001688f615d8440b674ab12199cd81916b209e
Killerqu00 <killerqueen1777@gmail.com>
2024-02-08 18:56:47 +0100

    ponders

---
+++ src/main/java/com/killerqu/compressiontweaks/ponder/PonderIndex.java
@@ -0,0 +1,12 @@
+package com.killerqu.compressiontweaks.ponder;
+
+import com.killerqu.compressiontweaks.CompressionTweaks;
+import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
+
+public class PonderIndex {
+    static final PonderRegistrationHelper HELPER = new PonderRegistrationHelper(CompressionTweaks.MODID);
+
+    public static void register() {
+        //HELPER.forComponents(BLOCK).addStoryBoard("SCHEMATICPATH", ClassName::function, PONDERTAG);
+    }
+}
