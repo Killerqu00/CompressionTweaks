307f535b94a38c0afcfc328cd7409a2a2e4adb14
Big Barza <francesco.b99@hotmail.it>
2025-06-15 16:26:20 +0200

    remove orphaned interface

--- src/main/java/com/killerqu/compressiontweaks/ponder/IPonderScene.java
+++
@@ -1,7 +0,0 @@
-package com.killerqu.compressiontweaks.ponder;
-
-import com.simibubi.create.foundation.ponder.PonderScene;
-
-public interface IPonderScene {
-    public PonderScene.SceneCamera getCamera();
-}
